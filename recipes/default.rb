4ca6473c3a51b91dabdedd6ddf437311bb77ca89
Scott McLeod <smcleod@zeddworks.com>
2011-08-03 13:45:09 -0400

    Run as a user, by default activemq

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,6 +23,14 @@ tmp = Chef::Config[:file_cache_path]
 version = node['activemq']['version']
 mirror = node['activemq']['mirror']
 activemq_home = "#{node['activemq']['home']}/apache-activemq-#{version}"
+activemq_user = node['activemq']['user']
+activemq_group = node['activemq']['group']
+
+user activemq_user do
+  comment "activemq user"
+  shell "/bin/bash"
+  home activemq_home
+end
 
 unless File.exists?("#{activemq_home}/bin/activemq")
   remote_file "#{tmp}/apache-activemq-#{version}-bin.tar.gz" do
@@ -33,11 +41,11 @@ unless File.exists?("#{activemq_home}/bin/activemq")
   execute "tar zxf #{tmp}/apache-activemq-#{version}-bin.tar.gz" do
     cwd "/opt"
   end
+
+  execute "chown -R #{activemq_user}:#{activemq_group} /opt/apache-activemq-#{version}"
 end
 
 file "#{activemq_home}/bin/activemq" do
-  owner "root"
-  group "root"
   mode "0755"
 end
 
@@ -48,6 +56,8 @@ link "/etc/init.d/activemq" do
   to "#{activemq_home}/bin/linux-#{arch}/activemq"
 end
 
+execute "sed -i 's/^#RUN_AS_USER=/RUN_AS_USER=#{activemq_user}/' /opt/apache-activemq-5.5.0/bin/linux-x86-64/activemq"
+
 service "activemq" do
   supports  :restart => true, :status => true
   action [:enable, :start]
@@ -56,6 +66,8 @@ end
 # symlink so the default wrapper.conf can find the native wrapper library
 link "#{activemq_home}/bin/linux" do
   to "#{activemq_home}/bin/linux-#{arch}"
+  owner activemq_user
+  group activemq_group
 end
 
 # symlink the wrapper's pidfile location into /var/run
@@ -66,7 +78,12 @@ end
 
 template "#{activemq_home}/bin/linux/wrapper.conf" do
   source "wrapper.conf.erb"
+  owner activemq_user
+  group activemq_group
   mode 0644
-  variables(:pidfile => "/var/run/activemq.pid")
+  variables(
+    :pidfile => "/var/run/activemq.pid",
+    :user => activemq_user
+  )
   notifies :restart, 'service[activemq]'
 end
